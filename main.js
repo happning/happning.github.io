aa6994420554b9f6f07a650d6237b18605304d09
Luc Gaitskell <me@lucg.xyz>
2023-12-05 14:03:23 -0500

    Apply auto-formatting

--- main.js
+++ main.js
@@ -1,108 +1,98 @@
-import './css/style.scss'
+import "./css/style.scss";
 // import AOS from 'aos';
 // import 'aos/dist/aos.css';
-import 'animate.css';
+import "animate.css";
 import gsap from "gsap";
 // import Lenis from '@studio-freight/lenis'
-import { ScrollTrigger } from 'gsap/all';
-import SplitType from 'split-type'
-import fullpage from 'fullpage.js';
+import { ScrollTrigger } from "gsap/all";
+import SplitType from "split-type";
+import fullpage from "fullpage.js";
 
 // if mobile
 const isMobile = () => {
-  return window.innerWidth < 1024
+  return window.innerWidth < 1024;
 };
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // cookie banner
-  const cookieBanner = document.querySelector('.cookieBanner');
-  const cookieBtn = document.querySelector('.closeCookie');
+  const cookieBanner = document.querySelector(".cookieBanner");
+  const cookieBtn = document.querySelector(".closeCookie");
   // check if cookie is set
-  if (localStorage.getItem('cookie') === 'true') {
-    cookieBanner.style.display = 'none'
+  if (localStorage.getItem("cookie") === "true") {
+    cookieBanner.style.display = "none";
   } else {
-    cookieBanner.style.display = 'flex'
+    cookieBanner.style.display = "flex";
   }
   // set cookie
-  cookieBtn.addEventListener('click', () => {
-    localStorage.setItem('cookie', 'true');
-    cookieBanner.style.display = 'none'
-  })
-
+  cookieBtn.addEventListener("click", () => {
+    localStorage.setItem("cookie", "true");
+    cookieBanner.style.display = "none";
+  });
 
   var myFullpage = new fullpage("#fullpage", {
     // Required for ScrollMagic to work
     scrollBar: true,
-    responsiveWidth: 800
-
+    responsiveWidth: 800,
   });
-
 
   gsap.registerPlugin(ScrollTrigger);
 
-  var sections = gsap.utils.toArray('.animateUp');
+  var sections = gsap.utils.toArray(".animateUp");
   sections.forEach((section) => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: section,
-        start: '30% 70%',
-        end: '-80% 30%',
+        start: "30% 70%",
+        end: "-80% 30%",
         markers: false,
         scrub: 2,
-      }
-    })
+      },
+    });
     //  up animation
     tl.from(section, {
       y: 150,
       opacity: 0,
-    })
-  })
+    });
+  });
 
-  var texts = gsap.utils.toArray('.textanim');
+  var texts = gsap.utils.toArray(".textanim");
 
   texts.forEach((text) => {
-    new SplitType(text)
+    new SplitType(text);
     const tl2 = gsap.timeline({
       scrollTrigger: {
         trigger: text,
-        start: isMobile ? '10% 80%' : 'center center',
-        end: isMobile ? '10% 80%' : 'center center',
+        start: isMobile ? "10% 80%" : "center center",
+        end: isMobile ? "10% 80%" : "center center",
         markers: false,
         scrub: 2,
-      }
-    })
+      },
+    });
 
     //  up animation
-    tl2.from(text.querySelectorAll('.word'), {
+    tl2.from(text.querySelectorAll(".word"), {
       y: 150,
       stagger: 0.4,
       duration: 1,
       opacity: 0,
-    })
+    });
+  });
 
-
-
-
-  })
-
-  var mocs = gsap.utils.toArray('.mocImage');
+  var mocs = gsap.utils.toArray(".mocImage");
   mocs.forEach((moc) => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: moc,
-        start: '10% 70%',
-        end: '-20% 30%',
+        start: "10% 70%",
+        end: "-20% 30%",
         markers: false,
         scrub: 2,
-      }
-    })
+      },
+    });
     //  up animation
     tl.from(moc, {
       y: 150,
       opacity: 0,
-    })
-  })
-
-
-
-})
+    });
+  });
+});
