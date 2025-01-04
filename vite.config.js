8877f680bc2359c7ac53f6aaad5cde61cdac7465
Luc Gaitskell <me@lucg.xyz>
2025-01-04 18:20:49 +0100

    Add app clip demo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  // your other configuration...
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        event: resolve(__dirname, "event/index.html"),
+      },
+    },
+  },
 });
